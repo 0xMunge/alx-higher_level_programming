4f37e3f2625723053fc803102571c1fc7ea1a0c8
0xMunge <munge@apeironconsult.com>
2023-01-23 16:28:32 +0300

    Py Py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for charachter in range(97, 123):
-        if (character != 101 and character != 113):
-                print("{:c}".fomat(haracter), end='')
+for character in range(97, 123):
+    if (character != 101 and character != 113):
+        print("{:c}".format(character), end='')
