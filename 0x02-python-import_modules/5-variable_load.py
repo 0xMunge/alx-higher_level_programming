f62f28bbf92a7a4bdbb26d9a31cd3c6e27e3204a
0xMunge <munge@apeironconsult.com>
2023-01-27 10:04:22 +0300

    import py py

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from variable_load_5 import a
-    print(a)
+if __name__ == '__main__':
+    import hidden_4
+    for item in dir(hidden_4):
+        if item[0:2] != "__":
+            print("{:s}".format(item))
