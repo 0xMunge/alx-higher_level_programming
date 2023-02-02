000a6979f675e4c77cc05d0c979bd82f531950ee
0xMunge <munge@apeironconsult.com>
2023-02-02 16:13:38 +0300

    Test-driven development

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-""" a module for a function that prints a square with the character #. """
+"""
+Module print_square
+Prints a square with the character #.
+"""
 
 
 def print_square(size):
-    """ function that prints a square with the character #.
-    Args:
-        size: type int the size of the squares side
-    Raises:
-        TypeError: type of ize must be int
-        ValueError: size must be > 0
+    """Prints a square where size is
+    the length and width of the square.
     """
+
     if type(size) is not int:
         raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
-    print(("#" * size + "\n") * size, end="")
 
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/4-print_square.txt")
+    for i in range(size):
+        for j in range(size):
+            print('#', end='')
+        print()
--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-""" a module for text indentaion method """
+"""
+Module text_indentation
+Adds two new lines after a set of characters.
+"""
 
 
 def text_indentation(text):
-    """ function to that prints a text with 2 new
-    lines after each of these characters: ., ? and :
-    Args:
-        text: type str text to be processed
-    Raises:
-        TypeError: text must be a string
+    """Prints text with added two newlines
+    after each of these characters {'.', '?', ':'}.
     """
-    if not isinstance(text, str):
+
+    if type(text) is not str:
         raise TypeError("text must be a string")
 
     for delim in ".:?":
         text = (delim + "\n\n").join(
             [line.strip(" ") for line in text.split(delim)])
 
-    print(text, end="")
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/5-text_indentation.txt")
+    print("{}".format(text), end="")
